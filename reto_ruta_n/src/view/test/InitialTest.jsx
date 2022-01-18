d0b8fc57bf6e22ad08d568a907fd18b62a798a72
tolozayurany <yalbarracint@gmail.com>
2022-01-18 18:42:44 -0500

    Se crea maquetación para pantalla de initial Test

--- reto_ruta_n/src/view/test/InitialTest.jsx
+++ reto_ruta_n/src/view/test/InitialTest.jsx
@@ -1,11 +1,29 @@
 // Vista de la pregunta inicial, ¿Tienes conocimientos en programación?
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 const InitialTest = () => {
+    const navigate = useNavigate();
     return (
-        <div>
-            InitialTest
-        </div>
+        <section className='section-initial-test'>
+            <div>
+                <p>
+                    Con este test tendrás al alcance de tu mano nuestras sugerencias para formarte como talento en el área de tecnología.
+                </p>
+            </div>
+            <div className='question-programmer'>
+                <p>
+                    ¿Tienes conocimientos en programación?
+                </p>
+                <div className='sect-buttons'>
+                    <button onClick={() => navigate('/TestProgrammer')}>
+                        SI
+                    </button>
+                    <button onClick={() => navigate('/TestUser')}>
+                        NO
+                    </button>
+                </div>
+            </div>
+        </section>
     );
 };
 
