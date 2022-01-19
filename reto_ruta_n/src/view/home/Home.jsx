0247974fc0c6c59ce53213f35a7ecb5faf49c2a8
Diana Buitrago <dianabuitrago54@hotmail.es>
2022-01-19 16:21:22 -0500

    Se estructura el ruteo eliminando la vista de registro y se configura el login con google, se empiezan estilos vista login

--- reto_ruta_n/src/view/home/Home.jsx
+++ reto_ruta_n/src/view/home/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import {Button} from '@chakra-ui/react';
 
 const Home = () => {
+    const navigate = useNavigate();
     return (
-        <div>
-            Home
-        </div>
+        <>
+            <h3>Hola </h3>
+            <p>¿Qué te gustaría hacer?</p>
+            <section className='sect-btn-home'>
+                <Button onClick={() => navigate('/InitialTest')}>Realizar test</Button>
+                <Button onClick={() => navigate('/')} >Resultados test anteriores</Button>
+            </section>
+        </>
     );
 };
-
 export default Home;
