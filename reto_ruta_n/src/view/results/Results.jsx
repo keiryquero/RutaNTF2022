2d162a5269c12165540cbd4548eaa8fe73a2f170
tolozayurany <yalbarracint@gmail.com>
2022-01-20 01:11:37 -0500

    Se crean estilos para  la pantalla de resultados

--- reto_ruta_n/src/view/results/Results.jsx
+++ reto_ruta_n/src/view/results/Results.jsx
@@ -1,58 +1,74 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import CourseList from "./CourseList";
+import { useNavigate } from 'react-router-dom';
 import {
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon,
-  Box,
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon,
+    Box,
+    Flex,
+    Button,
+    Heading
 } from "@chakra-ui/react";
 
 const Results = ({ path }) => {
- console.log('path', path)
+    console.log('path', path)
+    const navigate = useNavigate();
 
-  const [data, setDataState] = useState([]);
-  //console.log(data);
-  const getData = () => {
-    fetch("data.json")
-      .then((products) => products.json())
-      .then((data) => setDataState(data));
-  };
+    const [data, setDataState] = useState([]);
+    //console.log(data);
+    const getData = () => {
+        fetch("data.json")
+            .then((products) => products.json())
+            .then((data) => setDataState(data));
+    };
 
-  useEffect(() => {
-    getData();
-  }, []);
+    useEffect(() => {
+        getData();
+    }, []);
 
-  const filterRoute = path.flatMap(item=>data.filter(route => route.ruta.includes(item)));
- 
-  console.log('Diana', filterRoute)
+    const filterRoute = path.flatMap(item => data.filter(route => route.ruta.includes(item)));
 
-  return (
-     <div>
-    
-      {filterRoute.map((route) => (
-        <div key={route.id}>
-          <Accordion allowToggle>
-            <AccordionItem>
-              <h2>
-                <AccordionButton>
-                  <Box flex="1" textAlign="left">
-                    {route.ruta}
-                    {route.salario}
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4}>
-                <CourseList routeData={route} />
-              </AccordionPanel>
-            </AccordionItem>
-          </Accordion>
+    return (
+        <div>
+            <Flex bg='#00028A' direction='column' h='100vh' justifyContent='space-evenly'>
+                <Flex justifyContent='center'>
+
+                    <Heading as='h3' size='lg' margin='20px' color='white'>
+
+                        Estos son tus resultados
+                    </Heading >
+                </Flex>
+                {filterRoute.map((route) => (
+                    <div key={route.id}>
+                        <Flex bg='#00028A' direction='column' alignItems='center' justifyContent='center' >
+                            <Accordion allowToggle bg='#E5E5E5' w='85%' justifyContent='center' rounded='10px' position="relative" >
+                                <AccordionItem boxShadow='lg' rounded='10px' borderWidth='1px'>
+                                    <h2>
+                                        <AccordionButton>
+                                            <Flex flex="1" textAlign="left" pb={3} direction='column' alignItems='center' paddingTop='10px' fontWeight='bold' >
+                                                <div>{route.ruta}</div>
+                                                <div> Oferta salarial: {route.salario}</div>
+                                            </Flex>
+                                            <AccordionIcon />
+                                        </AccordionButton>
+                                    </h2>
+                                    <AccordionPanel pb={4}>
+                                        <CourseList routeData={route} />
+                                    </AccordionPanel>
+                                </AccordionItem>
+                            </Accordion>
+                        </Flex>
+                    </div>
+                ))}
+                <Flex justifyContent='center' w='100%'>
+                    <Button rounded='lg' marginTop='50px' marginBottom='20px' w='70%' onClick={() => navigate('/Home')}>REGRESAR</Button>
+                </Flex>
+            </Flex>
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 export default Results;
--- reto_ruta_n/src/view/results/Results2.jsx
+++ reto_ruta_n/src/view/results/Results2.jsx
@@ -8,10 +8,16 @@ import {
   AccordionPanel,
   AccordionIcon,
   Box,
+  Flex,
+  Button,
+  Heading
 } from "@chakra-ui/react";
+import { useNavigate } from 'react-router-dom';
 
 const Results2 = (porcRoutes) => {
- console.log('porcentajes', porcRoutes)
+  const navigate = useNavigate();
+
+  console.log('porcentajes', porcRoutes)
 
   const [data, setDataState] = useState([]);
   //console.log(data);
@@ -25,32 +31,50 @@ const Results2 = (porcRoutes) => {
     getData();
   }, []);
 
-  
+
   return (
-     <div>
-    
-      {data.map((route) => (
-        <div key={route.id}>
-          <Accordion allowToggle>
-            <AccordionItem>
-              <h2>
-                <AccordionButton>
-                  <Box flex="1" textAlign="left">
-                    {route.ruta}
-                    {porcRoutes.objRoutes[route.ruta]}
-                    {route.salario}
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4}>
-                <CourseList routeData={route} />
-              </AccordionPanel>
-            </AccordionItem>
-          </Accordion>
-        </div>
-      ))}
+    <div>
+      <Flex bg='#00028A' direction='column' justifyContent='space-evenly'>
+        <Flex justifyContent='center'>
+
+          <Heading as='h3' size='lg' margin='20px' color='white'>
+
+            Estos son tus resultados
+          </Heading >
+        </Flex>
+        {data.map((route) => (
+          <div key={route.id}>
+            <Flex color='white' marginTop='15px' justifyContent='center' w='85%' >
+              <h3>Afinidad con esta ruta: {porcRoutes.objRoutes[route.ruta]} %</h3>
+            </Flex>
+            <Flex bg='#00028A' direction='column' alignItems='center' justifyContent='center' >
+              <Accordion allowToggle bg='#E5E5E5' w='85%' justifyContent='center' rounded='10px' position="relative" >
+                <AccordionItem boxShadow='lg' rounded='10px' borderWidth='1px'>
+                  <h2>
+                    <AccordionButton  >
+                      <Flex flex="1" textAlign="left" pb={3} direction='column' alignItems='center' paddingTop='10px' fontWeight='bold' >
+                        <div>{route.ruta}</div>
+                        <div> Oferta salarial: {route.salario}</div>
+                      </Flex>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={1} >
+
+                    <CourseList routeData={route} />
+
+                  </AccordionPanel>
+                </AccordionItem>
+              </Accordion>
+            </Flex>
+          </div>
+        ))}
+        <Flex justifyContent='center' w='100%'>
+          <Button rounded='lg' marginTop='50px' marginBottom='20px' w='70%' onClick={() => navigate('/Home')}>REGRESAR</Button>
+        </Flex>
+      </Flex>
     </div>
+
   );
 };
 export default Results2;
